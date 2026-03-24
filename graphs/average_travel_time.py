cb79044c5390086b5f888836ba358653e8cdf62c
Arnav Jha <overrideeducation@gmail.com>
2026-03-23 22:34:05 -0700

    add graphs in matplotlib

--- graphs/average_travel_time.py
+++ graphs/average_travel_time.py
@@ -4,10 +4,10 @@ import numpy as np
 
 alpha = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 
-fixed =     [838, 831, 877, 909, 906, 852, 824, 791, 846, 895, 944]
-queue =     [955, 976, 986, 1024, 1024, 708, 889, 904, 903, 905, 830]
-classical = [908, 924, 949, 978, 996, 910, 938, 946, 926, 825, 763]
-quantum =   [855, 871, 923, 940, 1014, 1012, 923, 927, 843, 747, 680]
+fixed =     [105.85, 107.95, 105.65, 108.48, 108.78, 107.73, 112.06, 104.49, 98.41, 82.94, 72.19]
+queue =     [81, 81.94, 81.22, 82.75, 87.11, 104.13, 108.63, 100.86, 90.14, 79.44, 80.65]
+classical = [79.79, 79.79, 79.82, 82.11, 85.76, 100.28, 99.96, 99.54, 84.46, 84.61, 86.24]
+quantum =   [81.59, 81.77, 80.41, 83.04, 88.41, 91.45, 92.47, 98.08, 95.97, 95.91, 101.45]
 
 alpha_smooth = np.linspace(min(alpha), max(alpha), 1000)
 
@@ -28,11 +28,13 @@ plt.scatter(alpha, queue, s=50)
 plt.scatter(alpha, classical, s=50)
 plt.scatter(alpha, quantum, s=50)
 
-plt.xlabel('Probability of Straight (⍺)')
-plt.ylabel('Throughput')
-plt.title('Throughput vs Probability of Straight (⍺)')
+plt.xlabel('Probability of Straight (α)')
+plt.ylabel('Average Travel Time (s)')
+plt.title('Travel Time vs Probability of Straight (α)')
 
-plt.ylim(300, 1200)
+plt.ylim(50, 150)
+
+plt.gca().invert_yaxis()
 
 plt.legend()
 plt.grid()
--- graphs/average_waiting_time.py
+++ graphs/average_waiting_time.py
@@ -4,10 +4,10 @@ import numpy as np
 
 alpha = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 
-fixed =     [838, 831, 877, 909, 906, 852, 824, 791, 846, 895, 944]
-queue =     [955, 976, 986, 1024, 1024, 708, 889, 904, 903, 905, 830]
-classical = [908, 924, 949, 978, 996, 910, 938, 946, 926, 825, 763]
-quantum =   [855, 871, 923, 940, 1014, 1012, 923, 927, 843, 747, 680]
+fixed =     [44.43, 44.98, 44.27, 46.04, 45.21, 45.29, 48.31, 44.97, 45.97, 40.53, 39.47]
+queue =     [32.48, 32.19, 31.88, 31.83, 34.24, 37.86, 43.02, 41.51, 41.97, 38.49, 38.82]
+classical = [29.43, 29.22, 28.43, 29.43, 29.3, 32.99, 39.04, 40.2, 37.15, 37.32, 36.97]
+quantum =   [30.8, 30.93, 29, 30.48, 31.83, 32.26, 38.91, 39.73, 41.43, 42.15, 43.12]
 
 alpha_smooth = np.linspace(min(alpha), max(alpha), 1000)
 
@@ -28,11 +28,13 @@ plt.scatter(alpha, queue, s=50)
 plt.scatter(alpha, classical, s=50)
 plt.scatter(alpha, quantum, s=50)
 
-plt.xlabel('Probability of Straight (⍺)')
-plt.ylabel('Throughput')
-plt.title('Throughput vs Probability of Straight (⍺)')
+plt.xlabel('Probability of Straight (α)')
+plt.ylabel('Average Waiting Time (s)')
+plt.title('Waiting Time vs Probability of Straight (α)')
 
-plt.ylim(300, 1200)
+plt.ylim(15, 70)
+
+plt.gca().invert_yaxis()
 
 plt.legend()
 plt.grid()
