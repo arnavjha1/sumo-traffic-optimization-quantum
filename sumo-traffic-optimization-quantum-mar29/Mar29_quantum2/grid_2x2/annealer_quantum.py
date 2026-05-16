9b14462c1256e1ef69648067b7bf9112a174cfee
Arnav Jha <overrideeducation@gmail.com>
2026-05-16 14:25:31 -0700

    v2.0 - 90.03, 30.90, 1024

--- sumo-traffic-optimization-quantum-mar29/Mar29_quantum2/grid_2x2/annealer_quantum.py
+++ sumo-traffic-optimization-quantum-mar29/Mar29_quantum2/grid_2x2/annealer_quantum.py
@@ -2,45 +2,75 @@ from qiskit import QuantumCircuit
 from qiskit.primitives import StatevectorSampler
 import numpy as np
 
-def quantum_decision(biases, p=1):
+LAMBDA = 20
+
+def quantum_decision(biases, prev_state, p=1):
 
     n = len(biases)
 
-    # Parameters (we'll just grid search for now)
+    # Effective biases including switching penalty
+    effective_biases = []
+
+    for i in range(n):
+
+        # prev_state should be 0 or 1
+        penalty = LAMBDA * (1 - 2 * prev_state[i])
+
+        effective_biases.append(
+            biases[i] - penalty
+        )
+
     gammas = np.linspace(0, np.pi, 10)
     betas = np.linspace(0, np.pi/2, 10)
 
     best_energy = float("inf")
     best_bitstring = None
 
+    sampler = StatevectorSampler()
+
     for gamma in gammas:
         for beta in betas:
 
             qc = QuantumCircuit(n)
 
-            # Initialize superposition
+            # Superposition
             qc.h(range(n))
 
-            # ----- COST UNITARY -----
+            # COST UNITARY
             for i in range(n):
-                qc.rz(2 * gamma * biases[i], i)
+                qc.rz(
+                    2 * gamma * effective_biases[i],
+                    i
+                )
 
-            # ----- MIXER -----
+            # MIXER
             for i in range(n):
                 qc.rx(2 * beta, i)
 
             qc.measure_all()
 
-            sampler = StatevectorSampler()
             result = sampler.run([qc], shots=512).result()
             counts = result[0].data.meas.get_counts()
 
-            # Evaluate classical energy
             for bitstring, freq in counts.items():
-                bitstring = bitstring[::-1]
-                x = np.array([1 if b == '1' else 0 for b in bitstring])
 
-                energy = -np.dot(biases, x)
+                bitstring = bitstring[::-1]
+
+                x = np.array([
+                    1 if b == '1' else 0
+                    for b in bitstring
+                ])
+
+                # TRUE ENERGY
+                energy = 0
+
+                for i in range(n):
+
+                    energy += -biases[i] * x[i]
+
+                    energy += LAMBDA * (
+                        x[i] - prev_state[i]
+                    )**2
 
                 if energy < best_energy:
                     best_energy = energy
